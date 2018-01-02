b0f6069968d6531214869c79163a25c726ca8837
Darryl White <whitedarryl101@gmail.com>
2018-01-02 16:56:14 +0000

    rel path, font-display, added 512 asset, update nanifest

--- pomodoro_sw.js
+++ pomodoro_sw.js
@@ -1,21 +1,21 @@
-// v1.3
+// v1.5
 
 self.addEventListener('install', e => {
   e.waitUntil(
     caches.open('pomodoro').then(cache => {
       return cache.addAll([
-        '/',
-        '/manifest.json',
-        '/index.html',
-        '/scripts/main.js',
-        '/styles/main.css',
-        '/assets/fonts/gotham-medium.woff',
-        '/assets/images/pomodoro_256.png',
-        '/assets/images/pomodoro_144.png',
-        '/assets/images/pomodoro_64.png',
-        '/assets/images/volume_on.svg',
-        '/assets/images/volume_muted.svg',
-        '/assets/media/alarm.mp3'
+        '',
+        'manifest.json',
+        'index.html',
+        'scripts/main.js',
+        'styles/main.css',
+        'assets/fonts/gotham-medium.woff',
+        'assets/images/pomodoro_144.png',
+        'assets/images/pomodoro_256.png',
+        'assets/images/pomodoro_512.png',
+        'assets/images/volume_on.svg',
+        'assets/images/volume_muted.svg',
+        'assets/media/alarm.mp3'
       ])
       .then(() => self.skipWaiting());
     })
